010378485f85fed949902f613b1ef39ca96cd69a
ABAY, k <kassalalnyc24@gmail.com>
2025-05-18 14:46:35 -0400

    imort fixed

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Header from "../../components/Header/Header";
 import Banner from "../../components/Banner/Banner.jsx";
-import RowList from "../../Components/Rows/RowList/RowList.jsx";
+import RowList from "../../components/Rows/RowList/RowList.jsx";
 import Footer from "../../components/Footer/Footer";
 function Home() {
   return (
