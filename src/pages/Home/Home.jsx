e6432cf8dc2569982e6ef718d38a91e32e259948
ABAY, k <kassalalnyc24@gmail.com>
2025-05-18 08:10:19 -0400

    import fixed

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Header from "../../components/Header/Header";
 import Footer from '../../components/Footer/Footer';
 import Banner from "../../components/Banner/Banner.jsx";
-import RowList from "../../Components/Rows/RowList/RowList.jsx";
+import RowList from "../../Components/Rows/RowList/RowList";
 function Home() {
   return (
         <div>
